346a2ba3f4f855487a5ea9af4ca5fdab2afedbea
pricbnll <pricbnll@gmail.com>
2024-03-28 17:00:42 -0300

    feat: adicioando exercicio 4 da lista

--- semana9/app.js
+++ semana9/app.js
@@ -3,9 +3,6 @@
 const express = require("express")
 const app = express()
 
-app.listen(3000, () =>{
-    console.log("Servidor online do exercicício do trello - semana 9")
-})
 
 //[M1S09] Ex 2 - Rotas - Adicione rotas ao novo projeto: Uma rota GET que responda à URL '/sobre' com uma mensagem sobre o seu aplicativo. Uma rota GET que responda à URL '/contato' com uma mensagem de contato.
 
@@ -15,4 +12,19 @@ app.get("/sobre", function(req,res){
 
 app.get("/contato", (req,res) =>{
     res.send("Meu contato é por email@email.com")
+})
+
+// [M1S09] Ex 4 - Parâmetros - Defina uma rota GET que aceite um parâmetro de rota, como '/produto/:id', e responda com uma mensagem personalizada com base no ID do produto fornecido.
+
+app.get("/produto/:id", (req, res) => {
+    let id = req.params.id
+    res.send(`Você pesquisou pelo produto com id ${id}`)
+})
+
+//http://localhost:3000/produto/15
+// Você pesquisou pelo produto com id 15
+
+
+app.listen(3000, () =>{
+    console.log("Servidor online do exercicício do trello - semana 9")
 })
